4d1c775b7c71b77bcefa80ce25e0101ae854c4e1
Bruno Fidelis <bruno.fidelis004@gmail.com>
2016-11-13 09:05:56 -0200

    knn

--- algorithms/my_knn.py
+++ algorithms/my_knn.py
@@ -1,19 +1,10 @@
+from __future__ import division
 import numpy as np
 from math import sqrt
-import matplotlib.pyplot as plt
 import warnings
-from matplotlib import style
 from collections import Counter
-style.use('fivethirtyeight')
-
-dataset = {'k': [[1,2], [2, 3], [3, 1]], 'r': [[6,5], [7, 7], [8,6]]}
-new_features = [5, 7]
-
-# for i in dataset:
-# 	for ii in dataset[i]:
-# 		plt.scatter(ii[0], ii[1], s=100, color=i)
-# plt.scatter(new_features[0], new_features[1])
-# plt.show()
+import pandas as pd
+import random
 
 def knn(data, predict, k=3):
 	if len(data) >= k:
@@ -29,5 +20,33 @@ def knn(data, predict, k=3):
 	vote_result = Counter(votes).most_common(1) [0] [0]
 	return vote_result
 
-result = knn(dataset, new_features, k=3)
-print result
+df = pd.read_csv('datasets/breast-cancer-wisconsin.data.txt')
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
+
+random.shuffle(full_data)
+
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
+
+for i in train_data:
+	train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+	test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0 
+
+for group in test_set:
+	for data in test_set[group]:
+		vote = knn(train_set, data, k=5)
+		if group == vote:
+			correct +=1
+		total += 1
+
+print 'Accuracy:', correct/total
